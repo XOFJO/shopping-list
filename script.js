c2b873809fb892d52049563073dc94c5ed4515cb
Joseph Ding <dingyiheng91@gmail.com>
2023-03-03 22:44:04 -0500

    add filter

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const itemForm = document.querySelector("#item-form");
 const itemInput = document.querySelector("#item-input");
 const itemList = document.querySelector("#item-list");
+const clearBtn = document.querySelector("#clear");
+const filterDiv = document.querySelector(".filter");
+const filterInput = filterDiv.querySelector("input");
 
 function createIcon(classes) {
   const icon = document.createElement("i");
@@ -28,5 +31,50 @@ function addItem(e) {
   const button = createButton("remove-item btn-link text-red");
   li.appendChild(button);
   itemList.appendChild(li);
+  checkUI();
 }
+
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    e.target.parentElement.parentElement.remove();
+  }
+  checkUI();
+}
+
+function clearAll(e) {
+  while (itemList.firstElementChild) {
+    itemList.firstElementChild.remove();
+  }
+  checkUI();
+}
+
+function filterFn(e) {
+  const items = itemList.querySelectorAll("li");
+  const inputVal = e.target.value.toLowerCase();
+  items.forEach((item) => {
+    if (
+      item.firstChild.textContent.toLocaleLowerCase().indexOf(inputVal) === -1
+    ) {
+      item.style.display = "none";
+    } else {
+      item.style.display = "flex";
+    }
+  });
+}
+
+function checkUI() {
+  const item = itemList.querySelectorAll("li");
+  if (item.length == 0) {
+    filterDiv.style.display = "none";
+    clearBtn.style.display = "none";
+  } else {
+    filterDiv.style.display = "block";
+    clearBtn.style.display = "block";
+  }
+}
+
 itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearAll);
+filterInput.addEventListener("input", filterFn);
+checkUI();
