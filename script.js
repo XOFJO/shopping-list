38be1c02b7a4cdf913c6aa84eb27a03c90057be2
Joseph Ding <dingyiheng91@gmail.com>
2023-03-06 01:15:02 -0500

    Finish
    wait for fix (input error when update value while clear all items)

--- script.js
+++ script.js
@@ -4,6 +4,56 @@ const itemList = document.querySelector("#item-list");
 const clearBtn = document.querySelector("#clear");
 const filterDiv = document.querySelector(".filter");
 const filterInput = filterDiv.querySelector("input");
+const formBtn = itemForm.querySelector("button");
+let isEditMode = false;
+
+function removeFromStorage(item) {
+  let local = getItemFromStorage("item");
+  local = local.filter((i) => {
+    return i !== item;
+  });
+  console.log(local);
+  localStorage.setItem("item", JSON.stringify(local));
+}
+
+function getItemFromStorage(item) {
+  let local;
+  if (localStorage.getItem(item) === null) {
+    local = [];
+  } else {
+    local = JSON.parse(localStorage.getItem(item));
+  }
+  return local;
+}
+
+function loadItem() {
+  const storedVal = JSON.parse(localStorage.getItem("item"));
+  if (storedVal !== null) {
+    storedVal.forEach((item) => {
+      addItemToDom(item);
+    });
+  }
+}
+
+function addItemToDom(item) {
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(item));
+  const button = createButton("remove-item btn-link text-red");
+  li.appendChild(button);
+  itemList.appendChild(li);
+  checkUI();
+}
+
+function setItemEdit(item) {
+  isEditMode = true;
+  itemList.querySelectorAll("li").forEach((item) => {
+    item.classList.remove("editMode");
+  });
+  formBtn.innerHTML = "<i class='fa-solid fa-pen'></i> Update Item";
+  item.classList.add("editMode");
+  itemInput.value = item.textContent;
+  formBtn.style.backgroundColor = "#228B22";
+}
 
 function createIcon(classes) {
   const icon = document.createElement("i");
@@ -19,24 +69,51 @@ function createButton(classes) {
   return button;
 }
 
-function addItem(e) {
+function ifExist(item) {
+  const itemsFromStorage = getItemFromStorage("item");
+  return itemsFromStorage.includes(item);
+}
+
+function submitItem(e) {
   e.preventDefault();
-  const li = document.createElement("li");
+
   const newItem = itemInput.value;
   if (newItem === "") {
     alert("Enter value");
     return;
   }
-  li.appendChild(document.createTextNode(newItem));
-  const button = createButton("remove-item btn-link text-red");
-  li.appendChild(button);
-  itemList.appendChild(li);
-  checkUI();
+
+  if (ifExist(newItem)) {
+    alert("You have entered the same value!");
+    return;
+  }
+
+  if (isEditMode) {
+    const itemEditing = document.querySelector(".editMode");
+    removeFromStorage(itemEditing.textContent);
+    itemEditing.classList.remove("editMode");
+    itemEditing.remove();
+    isEditMode = false;
+  }
+
+  local = getItemFromStorage("item");
+  local.push(newItem);
+  localStorage.setItem("item", JSON.stringify(local));
+  addItemToDom(newItem);
+
+  formBtn.innerHTML = "<i class='fa-solid fa-plus'></i> Add Item";
+  formBtn.style.backgroundColor = "#333";
+  itemInput.value = "";
 }
 
-function removeItem(e) {
+function onClickItem(e) {
   if (e.target.parentElement.classList.contains("remove-item")) {
     e.target.parentElement.parentElement.remove();
+    removeFromStorage(
+      e.target.parentElement.parentElement.firstChild.textContent
+    );
+  } else {
+    setItemEdit(e.target);
   }
   checkUI();
 }
@@ -46,6 +123,7 @@ function clearAll(e) {
     itemList.firstElementChild.remove();
   }
   checkUI();
+  localStorage.removeItem("item");
 }
 
 function filterFn(e) {
@@ -72,9 +150,13 @@ function checkUI() {
     clearBtn.style.display = "block";
   }
 }
+function init() {
+  itemForm.addEventListener("submit", submitItem);
+  itemList.addEventListener("click", onClickItem);
+  clearBtn.addEventListener("click", clearAll);
+  filterInput.addEventListener("input", filterFn);
+  document.addEventListener("DOMContentLoaded", loadItem);
+  checkUI();
+}
 
-itemForm.addEventListener("submit", addItem);
-itemList.addEventListener("click", removeItem);
-clearBtn.addEventListener("click", clearAll);
-filterInput.addEventListener("input", filterFn);
-checkUI();
+init();
